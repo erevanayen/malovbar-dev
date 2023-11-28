30329cd7f488ea8642b9053b16ec8902ce5d7f15
erevanayen <zed@erevanayen.art>
2023-11-28 18:36:42 +0100

    feat: header wip

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,33 @@
+<header>
+	<a id="logo-link" href="#">malovbar</a>
+	<nav>
+		<a href="#">products</a>
+		<a href="#">about</a>
+	</nav>
+</header>
+
+<style lang="scss">
+	header {
+		position: relative;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #ffffff;
+		height: 64px;
+	}
+
+	#logo-link {
+	}
+
+	nav {
+		position: absolute;
+		right: 0px;
+
+  margin-right: 32px;
+
+  a:not(:last-child) {
+  margin-right: 32px;
+  }
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Header from '$lib/components/Header.svelte';
+	import '$lib/styles/style.scss';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>Hello, I'm the footer.</footer>
