1026fc7161b24924862d3b9d0e68ed68d16b8388
erevanayen <zed@erevanayen.art>
2023-12-03 09:28:10 +0100

    feat: changed styles preprocessing
    - now using svelte-preprocess instead of vite-preprocess
    - this way the styles can be put into separate files

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,28 +6,5 @@
 	</nav>
 </header>
 
-<style lang="scss">
-	header {
-		position: relative;
-
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		background-color: #ffffff;
-		height: 64px;
-	}
-
-	#logo-link {
-	}
-
-	nav {
-		position: absolute;
-		right: 0px;
-
-  margin-right: 32px;
-
-  a:not(:last-child) {
-  margin-right: 32px;
-  }
-	}
+<style src="./header.scss">
 </style>
